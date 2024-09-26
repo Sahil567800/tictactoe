28e3b18f2807241cb744693eae811f0bc497686e
Sahil Khan <sahilskp110@gmail.com>
2024-09-26 09:30:48 +0530

    first commit

---
+++ tictactoe.js
@@ -0,0 +1,188 @@
+let grid = document.querySelector(".grid");
+let cell = document.querySelectorAll(".cell");
+let startBtn = document.querySelector("#btn");
+let turn = "x";
+let isTurn = true;
+let isStart = false;
+let startSound = new Audio("./gameSound/gamestart.mp3");
+let cellClick = new Audio("./gameSound/click12.aac");
+let tieSound = new Audio("./gameSound/tie.mp3");
+let wrongClick = new Audio("./gameSound/wrongclickTrimmed.wav");
+let vic = new Audio("./gameSound/vicAud.mp3");
+startBtn.addEventListener("click", function () {
+  startSound.play();
+  isStart = true;
+});
+/*
+//non-optimize way to handle click 
+
+for (let i = 0; i < cell.length; i++) {
+  let currCell = cell[i];
+  currCell.addEventListener("click", function () {
+    console.log("clicked", currCell);
+
+    currCell.innerHTML = turn;
+    if (isTurn) {
+      turn = "y";
+      isTurn = false;
+    } else {
+      turn = "x";
+      isTurn = true;
+    }
+    let winner = checkWinner();
+    if (winner == 1) {
+      alert(" X is winner");
+     
+      resetgrid();
+    } else if (winner == 2) {
+      alert("Y is winner");
+      resetgrid();
+    }
+  });
+}
+*/ grid.addEventListener("click", function (event) {
+  let currrCell = event.target;
+  if (!isStart) {
+    alert("first click on start button");
+    return;
+  }
+  if (currrCell.innerHTML == "") {
+    cellClick.play();
+    currrCell.innerHTML = turn;
+    if (isTurn) {
+      turn = "o";
+  
+      isTurn = false;
+    } else {
+      turn = "x";
+      isTurn = true;
+    }
+  } else {
+    wrongClick.play();
+  }
+
+  
+  let winner = checkWinner();
+  if (winner == 1) {
+    // alert(" X is winner");
+    vic.play();
+    winnerGesture();
+
+    resetgrid();
+  } else if (winner == 2) {
+    alert("Y is winner");
+    vic.play();
+    winnerGesture();
+    resetgrid();
+  } else if (winner == 0) {
+    alert("game is draw");
+  }
+});
+
+function checkWinner() {
+  if (
+    (cell[0].innerHTML == "x" &&
+      cell[1].innerHTML == "x" &&
+      cell[2].innerHTML == "x") ||
+    (cell[3].innerHTML == "x" &&
+      cell[4].innerHTML == "x" &&
+      cell[5].innerHTML == "x") ||
+    (cell[6].innerHTML == "x" &&
+      cell[7].innerHTML == "x" &&
+      cell[8].innerHTML == "x") ||
+    (cell[0].innerHTML == "x" &&
+      cell[3].innerHTML == "x" &&
+      cell[6].innerHTML == "x") ||
+    (cell[1].innerHTML == "x" &&
+      cell[4].innerHTML == "x" &&
+      cell[7].innerHTML == "x") ||
+    (cell[2].innerHTML == "x" &&
+      cell[5].innerHTML == "x" &&
+      cell[8].innerHTML == "x") ||
+    (cell[0].innerHTML == "x" &&
+      cell[4].innerHTML == "x" &&
+      cell[8].innerHTML == "x") ||
+    (cell[2].innerHTML == "x" &&
+      cell[4].innerHTML == "x" &&
+      cell[6].innerHTML == "x")
+  ) {
+    return 1;
+  } else if (
+    (cell[0].innerHTML == "o" &&
+      cell[1].innerHTML == "o" &&
+      cell[2].innerHTML == "o") ||
+    (cell[3].innerHTML == "o" &&
+      cell[4].innerHTML == "o" &&
+      cell[5].innerHTML == "o") ||
+    (cell[6].innerHTML == "o" &&
+      cell[7].innerHTML == "o" &&
+      cell[8].innerHTML == "o") ||
+    (cell[0].innerHTML == "o" &&
+      cell[3].innerHTML == "o" &&
+      cell[6].innerHTML == "o") ||
+    (cell[1].innerHTML == "o" &&
+      cell[4].innerHTML == "o" &&
+      cell[7].innerHTML == "o") ||
+    (cell[2].innerHTML == "o" &&
+      cell[5].innerHTML == "o" &&
+      cell[8].innerHTML == "o") ||
+    (cell[0].innerHTML == "o" &&
+      cell[4].innerHTML == "o" &&
+      cell[8].innerHTML == "o") ||
+    (cell[2].innerHTML == "o" &&
+      cell[4].innerHTML == "o" &&
+      cell[6].innerHTML == "o")
+  ) {
+    return 2;
+  } else if (
+    cell[0].innerHTML !=
+    
+    "" &&
+    cell[1].innerHTML != "" &&
+    cell[2].innerHTML != "" &&
+    cell[3].innerHTML != "" &&
+    cell[4].innerHTML != "" &&
+    cell[5].innerHTML != "" &&
+    cell[6].innerHTML != "" &&
+    cell[7].innerHTML != "" &&
+    cell[8].innerHTML != ""
+  ) {
+    tieSound.play();
+
+    return 0;
+  }
+}
+function resetgrid() {
+  for (let i = 0; i < cell.length; i++) {
+    setTimeout(() => {
+      cell[i].innerHTML = "";
+    }, 1000);
+  }
+}
+
+function winnerGesture() {
+  const container = document.querySelector(".lottie");
+  container.style.display = "block";
+  grid.style.display = "none";
+  setTimeout(() => {
+    restartgame();
+    container.style.display = "none";
+  }, 3000);
+
+  const dotLottie = lottie.loadAnimation({
+    container: container,
+    autoplay: true,
+    loop: true,
+    renderer: "svg", // Use "svg" or "canvas" depending on your needs
+    path: "Animat1724468413277.json",
+  });
+  dotLottie.addEventListener("complete", function () {
+    container.style.display = "none";
+  });
+}
+
+function restartgame() {
+  isStart = true;
+  grid.style.display = "block";
+  location.reload();
+}
